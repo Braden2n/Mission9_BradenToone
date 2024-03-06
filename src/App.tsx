c6993b1337efd8963a138217e48b46635bfe28cd
btoone <braden@toonetown.com>
2024-03-06 11:23:02 -0700

    Added Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Heading() {
+  return (
+    <div className="Heading">
+      <h1>Welcome to the NCAA Basketball March Madness Website!</h1>
+      <h3>
+        This website contains all of the team information for the upcoming March
+        Madness Tournament.
+      </h3>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
     </div>
   );
 }
